a6e3ff3a0267b9c784aa9c8860e28ff411e80ee5
Anas Ali Khan <anasalikhan951@gmail.com>
2025-07-06 20:26:23 +0530

    Update AS_04_Task_1.py

--- Assignment 4/AS_04_Task_1.py
+++ Assignment 4/AS_04_Task_1.py
@@ -5,7 +5,10 @@ try:
     print('Reading file content:')
     for line in f1:
         count += 1
-        print(f'Line {count}: {line}')
+        l=line.strip()
+        print(f'Line {count}: {l}')
+
     f.close()
 except FileNotFoundError:
     print('Error: The file', "'sample.txt'", 'was not found.')
+
